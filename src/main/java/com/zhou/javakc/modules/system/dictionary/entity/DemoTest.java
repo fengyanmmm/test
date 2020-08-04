611a887d08916f1c16415b51b426d048ccda74d5
javakc <javakc@javakc.com>
2020-08-04 13:58:43 +0800

    test

--- src/main/java/com/zhou/javakc/modules/system/dictionary/entity/DemoTest.java
+++ src/main/java/com/zhou/javakc/modules/system/dictionary/entity/DemoTest.java
@@ -3,5 +3,6 @@ package com.zhou.javakc.modules.system.dictionary.entity;
 public class DemoTest {
     private String name;
     private String dev_1;
+    private String test;
     private String javakc79;
 }
